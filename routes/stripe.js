3bfb179d89f7b2824cdcdbdc7218aa7f0e7203db
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-21 13:52:32 -0300

    Stripe payment functionality added. Use of secret keys in .env file

---
+++ routes/stripe.js
@@ -0,0 +1,18 @@
+const router = require("express").Router()
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+
+router.post("/payment", (req, res) => {
+    stripe.charges.create({ //CREATING A CHARGE
+        source: req.body.tokenId, //INCLUDING SOURCE AND THE TOKENID FROM STRIPE
+        amount: req.body.amount,
+        currency: "usd",
+    }, (stripeErr, stripeRes) => { //RETURNING EITHER ERROR OR SUCCESS RESPONSE
+        if(stripeErr){
+            res.status(500).json(stripeErr)
+        } else {
+            res.status(200).json(stripeRes)
+        }
+    })
+})
+
+module.exports = router
