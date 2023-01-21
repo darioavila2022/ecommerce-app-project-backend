0e008bb2546b699f7750e1ed27ae012fa0a8e69e
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-21 13:39:10 -0300

    Mongodb connection

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const express = require("express")
+const app = express ()
+const mongoose = require ("mongoose")
+const dotenv = require ("dotenv")
+const cors = require ("cors")
+
+dotenv.config()
+
+mongoose
+.connect(process.env.MONGO_URL)
+    .then(() => console.log("Mongo database connected"))
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Backend server running");
+    })
