da236b30db1572d3b003d9929f6439f6bb3ac273
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-23 18:40:26 -0300

    Format mod

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.Schema(
         title: { type: String, required: true, unique: true },
         desc: { type: String, required: true },
         img: { type: String, required: true },
-        categ: { type: String },
+        categories: { type: Array },        
         brand: { type: String },
         year: { type: Number },
         price: { type: Number, required: true },        
