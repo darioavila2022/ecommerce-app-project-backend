d4dede8af652e3bc199ab1030f1f6c56b922fbc9
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-23 14:52:45 -0300

    Minor product model mod

--- models/Product.js
+++ models/Product.js
@@ -5,9 +5,9 @@ const ProductSchema = new mongoose.Schema(
         title: { type: String, required: true, unique: true },
         desc: { type: String, required: true },
         img: { type: String, required: true },
-        categories: { type: Array },
+        categ: { type: string },
         brand: { type: String },
-        year: { type: Array },
+        year: { type: Number },
         price: { type: Number, required: true },        
     },
     { timestamps: true }
