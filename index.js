2814862a1b5bef750a124435382c943b219274b1
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-30 01:20:01 -0300

    Access-Control-Allow-Origin header added

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ mongoose.connect(process.env.MONGO_URI)
         console.log(err);
     })
 
+    app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "https://ecommerce-app.up.railway.app/");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+      });
 
 app.use(cors());
 app.use(express.json())
